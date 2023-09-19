b686a1025639b82d1027b74daa369a32954c248c
ycambro <110075830+ycambro@users.noreply.github.com>
2023-09-19 15:08:37 -0600

    Actualizar ActualizarClienteServlet.java

--- project1_BD2/src/main/java/com/mycompany/project1_bd2/Servlets/ActualizarClienteServlet.java
+++ project1_BD2/src/main/java/com/mycompany/project1_bd2/Servlets/ActualizarClienteServlet.java
@@ -9,10 +9,11 @@ import com.mycompany.project1_bd2.Repositorios.ClienteRepositorio;
 import com.mycompany.project1_bd2.entidades.Cliente;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher; 
+import jakarta.servlet.ServletException; 
+import jakarta.servlet.http.HttpServlet; 
+import jakarta.servlet.http.HttpServletRequest; 
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
